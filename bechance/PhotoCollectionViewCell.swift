cb18100615058d5fb1f503c67ede76f28da46566
Taiowa Waner <t.waner@gmail.com>
2015-09-20 16:41:50 -0400

    feat: UI Updates & Custom CollectionView Class
    Updates for custom collection view cell

---
+++ bechance/PhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCollectionViewCell.swift
+//  bechance
+//
+//  Created by Taiowa Waner on 9/20/15.
+//  Copyright © 2015 Taiowa Waner. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+}
---
+++ bechance/TaskCancellingCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TaskCancellingCollectionViewCell.swift
+//  bechance
+//
+//  Created by Taiowa Waner on 9/20/15.
+//  Copyright © 2015 Taiowa Waner. All rights reserved.
+//
+
+import UIKit
+
+import UIKit
+
+class TaskCancellingTableViewCell: UICollectionViewCell {
+    var imageName: String = ""
+    
+    var taskToCancelIfCellIsReused: NSURLSessionTask? {
+        didSet {
+            if let taskToCancel = oldValue {
+                taskToCancel.cancel()
+            }
+        }
+    }
+}
